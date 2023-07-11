6d97b4d9fb2e7efee2c25d9cb630534861fab0d7
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-11 01:56:14 +0100

    Update API.java

--- API.java
+++ API.java
@@ -4,42 +4,51 @@
  */
 package projetird;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import java.io.File;
 
-/**
- *
- * @author Houda
- */
 @RestController
-public class API {
-    @PostMapping("/upload")
-    public ResponseEntity<String> handleFileUpload(@RequestPart("file") MultipartFile file) {
+public class ReseauNeurone {
+    private static final String cheminFichier = "C:\\Users\\Houda\\Desktop\\ProjetIRD\\model.h5";
+
+    @PostMapping("/predict")
+    public ResponseEntity<?> predict(@RequestParam("file") File file)  {
         try {
-            // Vérifiez si le fichier est vide
-            if (file.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le fichier est vide.");
-            }
-             String destinationPath = " C:/Users/Houda/Desktop/ProjetIRD/src/fichier.xml";
-            
-        
-        // Enregistrez le fichier XML sur le disque
-        byte[] fileBytes = file.getBytes();
-        Files.write(Paths.get(destinationPath), fileBytes);
-        XML xml=new XML(destinationPath);
-        xml.lireFichier();
-            // Répondre avec un message de succès
-            return ResponseEntity.ok("Fichier importé avec succès !");
+            // Extraire les données du fichier XML
+            XML filexml = new XML(file);
+            double[][][] donnees = filexml.lireFichierXML();
+
+            // Charger le modèle à partir du fichier HDF5 en utilisant la classe KerasModelImport de Deeplearning4j
+            ComputationGraph model = KerasModelImport.importKerasModelAndWeights(cheminFichier);
+
+            // Préparer les tensors d'entrée et de sortie
+            INDArray donneeEntree = Nd4j.create(donnees);
+            INDArray donneeSortie = model.outputSingle(donneeEntree);
+            System.out.println(donneeSortie);
+
+            // Convertir les prédictions en JSON
+            String jsonResult = convertToJson(donneeSortie);
+
+            return ResponseEntity.ok(jsonResult);
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur s'est produite lors de l'importation du fichier.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur s'est produite : " + e.getMessage());
         }
     }
-    
+
+    private String convertToJson(INDArray predictions) {
+        // Convertir les prédictions en JSON en utilisant une bibliothèque JSON (par exemple, Gson)
+        Gson gson = new Gson();
+        return gson.toJson(predictions.toDoubleMatrix());
+    }
 }
+
