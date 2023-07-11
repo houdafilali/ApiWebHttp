62e230e4adc22594d009cc4eb215d752d33fbb04
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-11 20:14:17 +0100

    Update importTensorflowAstf.py

--- importTensorflowAstf.py
+++ importTensorflowAstf.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
-
-# Chemin vers le fichier h5 contenant le modèle
-
-
 # Charger le modèle à partir du fichier h5
 modele = tf.keras.models.load_model("model.h5")
-
 # Afficher un résumé du modèle
 modele.summary()
