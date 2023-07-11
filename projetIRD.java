d528e13eb192a18a0d6c5983bc247255afb052b1
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-11 01:51:42 +0100

    Update projetIRD.java

--- projetIRD.java
+++ projetIRD.java
@@ -4,10 +4,6 @@
  */
 package projetird;
 
-import com.sun.tools.javac.Main;
-import java.io.IOException;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,12 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author Houda
  */
-public class projetIRD {
-      public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, Exception {
-            // TODO code application logic here
-             SpringApplication.run(Main.class, args);
-            
-           
-          
+@SpringBootApplication
+public class ApiWebHttpApplication {
+
+    public static void main(String[] args)  {
+        SpringApplication.run(ApiWebHttpApplication.class, args);
     }
+
 }
