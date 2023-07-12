33a252b29d011aec8aa342b7965d4f67f734611b
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-12 05:46:57 +0100

    Update API.java

--- API.java
+++ API.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projetird;
 
-
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,42 +13,85 @@ import org.springframework.web.bind.annotation.RestController;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import java.io.File;
-
+import java.io.IOException;
+/**
+ *
+ * @author Houda
+ */
 @RestController
 public class ReseauNeurone {
-    private static final String cheminFichier = "C:\\Users\\Houda\\Desktop\\ProjetIRD\\model.h5";
+    private static final String cheminFichier = "model.h5";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @PostMapping("/predict")
     public ResponseEntity<?> predict(@RequestParam("file") File file)  {
         try {
-            // Extraire les données du fichier XML
-            XML filexml = new XML(new File("C:\\Users\\Houda\\Desktop\\ProjetIRD\\matrices.xml"));
-             double[][][] donnees = filexml.lireFichierXML();
-            // Charger le modèle à partir du fichier HDF5 en utilisant la classe KerasModelImport de Deeplearning4j
-            ComputationGraph model = KerasModelImport.importKerasModelAndWeights("C:\\Users\\Houda\\Desktop\\ProjetIRD\\model.h5");
-                // Préparer les tensors d'entrée et de sortie
-                int numDonnees = donnees.length;
-                int numSorties = 2;
-                double[][] predictions = new double[numDonnees][numSorties];
-        
-                for (int i = 0; i < numDonnees; i++) {
-                    INDArray donneesEntrees = Nd4j.create(donnees[i]);
-                    INDArray donneesSorties = model.outputSingle(donneesEntrees);
-                    predictions[i] = donneesSorties.toDoubleVector();
-                }
 
-            String jsonResult = convertToJson(predictions);
+            XML filexml = new XML(file);
+            //extraire les donneees du fichier XML
+            double[][][] donnees = filexml.lireFichierXML();
 
-            return ResponseEntity.ok(jsonResult);
+            // Load the pre-trained model
+            ClassLoader classLoader = getClass().getClassLoader();
+            File modelFile = new File(classLoader.getResource(cheminFichier).getFile());
+            String MODEL_PATH = modelFile.getAbsolutePath();
+
+            // Vérification de la présence du fichier et du chargement du modèle
+            if (!new File(MODEL_PATH ).exists()) {
+                System.out.println("Le fichier modèle n'a pas été trouvé : " + MODEL_PATH);
+                return null; // ou lancez une exception appropriée
+            }
+
+            // Chargement du modèle
+            ComputationGraph model;
+            try {
+                model = KerasModelImport.importKerasModelAndWeights(MODEL_PATH,false);
+            } catch (IOException | InvalidKerasConfigurationException | UnsupportedKerasConfigurationException e) {
+                System.out.println("Erreur lors du chargement du modèle : " + e.getMessage());
+                e.printStackTrace();
+                return null; // ou lancez une exception appropriée
+            }
+
+            // Vérification du modèle chargé
+            if (model == null) {
+                System.out.println("Le modèle n'a pas été chargé avec succès.");
+                return null; // ou lancez une exception appropriée
+            }
+
+            // Préparer les tenseurs d'entrée et de sortie
+            int numDonnees = donnees.length;
+            if (numDonnees == 0) {
+                System.out.println("Les données d'entrée sont vides.");
+                return null;
+            }
+            INDArray donneesEntrees = Nd4j.create(donnees);
+
+            // Make predictions
+            INDArray[] donneesSorties = model.output(donneesEntrees);
+
+            // Convert the predictions to a 2D float array
+            double[][] predictions = new double[donneesSorties.length][];
+            for (int i = 0; i < donneesSorties.length; i++) {
+                INDArray output = donneesSorties[i];
+                predictions[i] = output.toDoubleVector();
+            }
+
+            // Convert predictions to JSON
+            String json = objectMapper.writeValueAsString(predictions);
+
+            // Return the predictions as JSON
+            return ResponseEntity.ok().body(json);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur s'est produite : " + e.getMessage());
         }
     }
 
-    private String convertToJson(double [][] predictions) {
-        // Convertir les prédictions en JSON en utilisant une bibliothèque JSON (par exemple, Gson)
-        Gson gson = new Gson();
-        return gson.toJson(predictions);
-    }
+
 }
+
+
+
+
+
+
 
