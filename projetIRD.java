69029b75d3edf5efaba38e2d983dea065606427c
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-12 21:46:55 +0100

    Update projetIRD.java

--- projetIRD.java
+++ projetIRD.java
@@ -16,6 +16,14 @@ public class projetIRD {
     public static void main(String[] args) {
        SpringApplication.run(projetIRD.class, args);
     }
+      @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // Augmentez la limite de taille maximale ici (en octets)
+        factory.setMaxFileSize(DataSize.ofBytes(20 * 1024 * 1024));
+        factory.setMaxRequestSize(DataSize.ofBytes(20 * 1024 * 1024));
+        return factory.createMultipartConfig();
+    }
 
 }
 
