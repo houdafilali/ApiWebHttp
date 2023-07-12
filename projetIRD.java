ad1d232ff9a0d8e4daf43f60196c2fcfb03f4416
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-12 22:46:06 +0100

    Update projetIRD.java

--- projetIRD.java
+++ projetIRD.java
@@ -1,8 +1,12 @@
 package projetird;
 
 
+import jakarta.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 
 
 
