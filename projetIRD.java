24195f7edeba9535fffb11c4cd4695e692f79485
houdafilali <103257074+houdafilali@users.noreply.github.com>
2023-07-12 05:45:39 +0100

    Update projetIRD.java

--- projetIRD.java
+++ projetIRD.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projetird;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,8 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiWebHttpApplication {
 
-    public static void main(String[] args)  {
-        SpringApplication.run(ApiWebHttpApplication.class, args);
+    public static void main(String[] args) {
+       SpringApplication.run(ApiWebHttpApplication.class, args);
     }
+
+}
 
 }
